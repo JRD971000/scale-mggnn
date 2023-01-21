6f1c2929f9c3e1f1293c60832e2c57204fe93825
Ali Taghibakhshi <jrd@gra-login3.graham.sharcnet>
2023-01-21 16:37:10 -0500

    modify run

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-scottmac
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=150000               # memory per node
-#SBATCH --time=0-12:00
+#SBATCH --mem=50000               # memory per node
+#SBATCH --time=0-1:00
 
 source ../mlvl-mloras/bin/activate
 python train-gpu.py --num-epoch = 2 &> output.txt                      # you can use 'nvidia-smi' for a test
