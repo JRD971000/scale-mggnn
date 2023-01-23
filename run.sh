a66aaedd0bdc1433139a09789f408759bc34091e
ali taghibakhshi <alitaghibakhshi@wirelessprv-10-192-155-204.near.illinois.edu>
2023-01-23 16:53:10 -0600

    venv setup

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 #SBATCH --mem=50000               # memory per node
 #SBATCH --time=0-1:00
 
-source ../mlvl-mloras/bin/activate
+source testEnv/bin/activate
 python train-gpu.py --num-epoch = 2 &> output.txt                      # you can use 'nvidia-smi' for a test
