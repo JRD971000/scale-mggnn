6ea33f767c5c5f2b179c7ba91e40b3a93e3584fb
ali taghibakhshi <alitaghibakhshi@wirelessprv-10-192-155-204.near.illinois.edu>
2023-01-24 16:58:08 -0600

    make env

--- createEnv.sh
+++ createEnv.sh
@@ -9,6 +9,11 @@ source testEnv/bin/activate
 pip install --upgrade pip
 pip install wheel
 pip install -r reqs.txt
+pip install pygmsh
+pip install gmsh-sdk
+pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install pyg-lib torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
+pip install torch-geometric
 
 echo
 echo ---
