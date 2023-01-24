4e2fd20142c81606b327031df5ac3d6a8097045c
ali taghibakhshi <alitaghibakhshi@wirelessprv-10-192-155-204.near.illinois.edu>
2023-01-23 18:03:11 -0600

    venv setup

---
+++ makeNumml.sh
@@ -0,0 +1,15 @@
+module purge
+module load cuda/11.7
+module load python/3.8
+module load scipy-stack
+
+export CUDA_VISIBLE_DEVICES=0
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
+nvidia-cuda-mps-control -d
+
+cp -r testEnv $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+cp -r numml $SLURM_TMPDIR/numml
+pip3 install $SLURM_TMPDIR/numml/.
