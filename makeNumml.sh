862e3005914bb0107383c2d6ad450faa3d6eea53
ali taghibakhshi <alitaghibakhshi@alis-MacBook-Pro-2.local>
2023-01-25 23:02:38 -0600

    create env

--- makeNumml.sh
+++ makeNumml.sh
@@ -1,7 +1,7 @@
 module purge
-module load cuda/11.7
-module load python/3.8
-module load scipy-stack
+#module load cuda/11.7
+#module load python/3.8
+#module load scipy-stack
 
 export CUDA_VISIBLE_DEVICES=0
 export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
