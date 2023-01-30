a676b39a41b58e588c3ea587a3513d01c08a0ce4
Nicolas Nytko <nnytko2@illinois.edu>
2023-01-30 13:58:10 -0500

    Get numml installed on narval

--- createEnv.sh
+++ createEnv.sh
@@ -4,16 +4,19 @@
 
 module load cuda/11.7
 module load python/3.8
+
 python3 -m venv testEnv
 source testEnv/bin/activate
+
 pip install --upgrade pip
 pip install wheel
 pip install -r reqs.txt
 pip install pygmsh
 pip install gmsh-sdk
-pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install pyg-lib torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
-pip install torch-geometric
+
+#pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
+#pip install pyg-lib torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
+#pip install torch-geometric
 
 echo
 echo ---
--- makeNumml.sh
+++ makeNumml.sh
@@ -1,7 +1,7 @@
 module purge
 module load cuda/11.7
 module load python/3.8
-#module load scipy-stack
+module load scipy-stack
 
 export CUDA_VISIBLE_DEVICES=0
 export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
@@ -9,7 +9,9 @@ export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
 nvidia-cuda-mps-control -d
 
 cp -r testEnv $SLURM_TMPDIR/env
-sourse $SLURM_TMPDIR/env/bin/activate
+source $SLURM_TMPDIR/env/bin/activate
+
+pip3 install --no-index torch torch_cluster torch_geometric torch_scatter torch_sparse
 
 cp -r numml $SLURM_TMPDIR/numml
 pip3 install $SLURM_TMPDIR/numml/.
